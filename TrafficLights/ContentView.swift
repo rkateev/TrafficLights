4719e7d11943076fd02b54c7b11b67dae30acb06
rkateev <60319058+rkateev@users.noreply.github.com>
2021-10-28 04:55:17 +0300

    added button

--- TrafficLights/ContentView.swift
+++ TrafficLights/ContentView.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var buttonName = "Start"
     @State private var tapCount = 0
     @State private var redLight = 0.3
     @State private var yellowLight = 0.3
@@ -19,17 +20,37 @@ struct ContentView: View {
                 .foregroundColor(.red)
                 .frame(width: 100, height: 100)
                 .opacity(redLight)
+                
             Circle()
                 .foregroundColor(.yellow)
                 .frame(width: 100, height: 100)
                 .opacity(yellowLight)
+               
             Circle()
                 .foregroundColor(.green)
                 .frame(width: 100, height: 100)
                 .opacity(greenLight)
             Spacer()
-            Button(action: {}) {
-                Text("Tap")
+            
+            Button(action: {
+                buttonName = "Next"
+                let colors = (redLight, yellowLight, greenLight)
+                switch colors{
+                case (1, _, _):
+                    redLight = 0.3
+                    yellowLight = 1
+                case (_, 1, _):
+                    yellowLight = 0.3
+                    greenLight = 1
+                case (_, _ , 1):
+                    greenLight = 0.3
+                    redLight = 1
+                default:
+                    redLight = 1
+                }
+            }
+            ) {
+                Text(buttonName)
                     .font(.largeTitle)
             }
             }
