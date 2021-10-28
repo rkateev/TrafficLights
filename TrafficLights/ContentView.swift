dc4baeff2f2e8ff359db4fb0cf452d867a188ea6
rkateev <60319058+rkateev@users.noreply.github.com>
2021-10-28 03:40:13 +0300

    added interface

--- TrafficLights/ContentView.swift
+++ TrafficLights/ContentView.swift
@@ -8,14 +8,41 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var tapCount = 0
+    @State private var redLight = 0.3
+    @State private var yellowLight = 0.3
+    @State private var greenLight = 0.3
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
-    }
+        VStack {
+            Circle()
+                .foregroundColor(.red)
+                .frame(width: 100, height: 100)
+                .opacity(redLight)
+            Circle()
+                .foregroundColor(.yellow)
+                .frame(width: 100, height: 100)
+                .opacity(yellowLight)
+            Circle()
+                .foregroundColor(.green)
+                .frame(width: 100, height: 100)
+                .opacity(greenLight)
+            Spacer()
+            Button(action: {}) {
+                Text("Tap")
+                    .font(.largeTitle)
+            }
+            }
+        .padding()
+        }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
+        Group {
+            ContentView()
+        }
         ContentView()
+            .previewInterfaceOrientation(.landscapeLeft)
     }
 }
